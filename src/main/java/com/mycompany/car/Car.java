e5b318bb16f66dbdb12ca63be253fad7f58fd507
Mihajlo <mihajlo0709@gmail.com>
2021-03-12 14:37:22 +0100

    Car simple

--- src/main/java/com/mycompany/car/Car.java
+++ src/main/java/com/mycompany/car/Car.java
@@ -3,32 +3,30 @@ package com.mycompany.car;
 
 
 public class Car {
+
     public String brand;
     public String model;
     public String color;
     private int buildYear;
     
-    public Car() {
-        
-    } 
+    public int getbuildYear() {
+    return this.buildYear;
+   }
     
-    public Car(int buildYear) {
-        this.buildYear = buildYear;
-    }
+ 
     public Car(String brand, String model, String color, int buildYear) {
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.buildYear = buildYear;
+        
     }
     
-    public int getBuildYear() {
-        return this.buildYear;
-      }  
-    public void printTtributes() {
+    public void printAttributes() {
         System.out.println("Brand: " + this.brand);
         System.out.println("Model: " + this.model);
         System.out.println("Color: " + this.color);
-        System.out.println("build year: " + this.getBuildYear());
-    }  
-}
+        System.out.println("Build year: " + this.getbuildYear());
+    }
+
+ }
--- src/main/java/com/mycompany/car/Main.java
+++ src/main/java/com/mycompany/car/Main.java
@@ -5,24 +5,12 @@ package com.mycompany.car;
 public class Main {
 
     public static void main(String[] args) {
-        Car audi = new Car();
-        audi.brand = "Audi";
-        audi.model = "A4";
-        audi.color = "Black";
-        audi.printTtributes();
-    
-        Car bmw = new Car();
-        bmw.brand = "BMW";
-        bmw.model = "X5";
-        bmw.color = "Red";
-        bmw.printTtributes();
-    
-    
-        Car fiat = new Car(2010);
-        System.out.println("Godina proizvodnje: " + fiat.getBuildYear());
-    
-        Car lada = new Car("Lada", "Niva", "green", 2018);
-        lada.printTtributes();
+        
+        Car audi = new Car("Audi", "A4", "Black", 2010);
+        audi.printAttributes();
+        
+        
+        Car bmw = new Car("BMW", "X5", "Red", 2018);
+        bmw.printAttributes();
     }
-    
 }
